bacc9e6615d624fb4f4ad6e7589c6b23e405c33b
YokoYolo <y.ravlyk@gmail.com>
2018-07-20 17:56:03 -0400

    wow

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Item = require('../models/item');
+const Post = require('../models/post');
 
 // const dbName = 'miss-yoko-beading'
 mongoose.connect(process.env.MONGODB_URI);
@@ -34,16 +35,36 @@ const items = [
     }
 
 ]
-Item.create(items)
+
+
+
+
+
+  const posts = [
+      {
+          title: 'Peyote Stitch Beading: A Tutorial for Getting Started',
+          image: 'https://res.cloudinary.com/miss-yoko-beading/image/upload/v1532097543/folder-name/ja_delia.jpg.jpg',
+          description: 'You may be familiar with bead weaving – either you’ve given it a go yourself, or you’ve seen other people’s work. Bead-woven work can be to be terribly daunting; it appears intricate and complex, and it certainly can be time-consuming! However, please don’t let first impressions put you off having a go. Yes, bead weaving takes time and patience, but it is very much worth the effort!',
+          shortdescription: 'About Peyote Stitch technique',
+      },
+  
+  ]
+  
+  Item.create(items)
   .then ((result)=>{
+    Post.create(posts)
+    .then ((result)=>{
+        console.log (`created ${result.length} items`);
       console.log (`created ${result.length} items`);
       mongoose.disconnect();
   })
   .catch ((err)=>{
       console.log('error');
   })
-
-
-
+  
+  
+  
+  
+  
 
 
